f0ca8b5c8828947816ce9f4b38a9d905649459fc
Amanda Ström <amanda@mac.home>
2025-03-10 13:01:23 +0100

    Adding Angies LetterSearch

--- src/main/java/se/salt/chapter4/LetterSearch.java
+++ src/main/java/se/salt/chapter4/LetterSearch.java
@@ -3,10 +3,8 @@ package se.salt.chapter4;
 import java.util.Scanner;
 
 public class LetterSearch {
+    public static void main(String[] args){
 
-    public static void main(String args[]){
-
-        //Get text
         System.out.println("Enter some text:");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.next();
@@ -14,20 +12,20 @@ public class LetterSearch {
 
         boolean letterFound = false;
 
-        //Search text for letter A
+        //Search text for letter F
         for(int i=0; i<text.length(); i++){
             char currentLetter = text.charAt(i);
-            if(currentLetter == 'A' || currentLetter == 'a'){
+            if(currentLetter == 'F' || currentLetter == 'f'){
                 letterFound = true;
                 break;
             }
         }
 
         if(letterFound){
-            System.out.println("This text contains the letter 'A'");
+            System.out.println("This text contains the letter 'F'");
         }
         else{
-            System.out.println("This text does not contain the letter 'A'");
+            System.out.println("This text does not contain the letter 'F'");
         }
     }
 }
