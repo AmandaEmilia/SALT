6b99bbb947b1ed922299db96c509995e79274f6f
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-11 15:34:49 +0100

    Lottery ticket generator with int array

---
+++ src/main/java/se/salt/LotteryTicket.java
@@ -0,0 +1,30 @@
+package se.salt;
+
+import java.util.Random;
+
+public class LotteryTicket {
+
+    public static void main(String[] args) {
+
+        int[]lotteryTicket = generateLotteryNumbers();
+
+        printTicket(lotteryTicket);
+
+    }
+
+    public static int[] generateLotteryNumbers(){
+        Random random = new Random();
+        int[] numbers = new int[6];
+
+        for (int i = 0; i < 6; i++) {
+            numbers[i] = random.nextInt(1,69);
+        }
+        return numbers;
+    }
+
+    private static void printTicket(int[] lotteryTicket) {
+        for (int i = 0; i < generateLotteryNumbers().length; i++) {
+            System.out.print(lotteryTicket[i] + " - ");
+        }
+    }
+}
