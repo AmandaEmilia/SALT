237406cd9b76f50bbb4094954054d0b6fb3fe612
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-17 22:34:36 +0100

    Creating a superclass Cake

---
+++ src/main/java/se/salt/chapter9/Cake.java
@@ -0,0 +1,24 @@
+package se.salt.chapter9;
+
+public class Cake {
+
+    protected String flavor;
+
+    protected int price;
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
