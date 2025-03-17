a6553780652b2c9970d545a4b32d70480eff886f
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-17 23:01:09 +0100

    Adding some printouts for super/subclasses

--- src/main/java/se/salt/chapter9/TasteTester.java
+++ src/main/java/se/salt/chapter9/TasteTester.java
@@ -2,7 +2,24 @@ package se.salt.chapter9;
 
 public class TasteTester {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        Cake cake = new Cake();
+        cake.setPrice(1000);
+        System.out.println("This cake has " + cake.getFlavor() + " flavor as stated in Cake");
+        System.out.println("This cake is $" + cake.getPrice() + " as we stated in this class");
+
+        System.out.println("----------");
+
+        BirthdayCake birthdayCake = new BirthdayCake();
+        System.out.println("This birthdaycake has " + birthdayCake.getFlavor() + " flavor as stated in BirthdayCake");
+        System.out.println("This birthdaycake is $" + birthdayCake.getPrice() + " as stated in BirthdayCake");
+
+        System.out.println("----------");
+
+        WeddingCake weddingCake = new WeddingCake();
+        System.out.println("This weddingCake has " + weddingCake.getFlavor() + " flavor as stated in weddingCake");
+        System.out.println("This weddingCake is $" + weddingCake.getPrice() + " as stated in weddingCake");
+
+
     }
 }
