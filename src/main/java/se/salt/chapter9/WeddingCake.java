a55d0418d2695c3c79924dea00470848b1bd2c3c
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-17 22:46:24 +0100

    Adding constructor values for WeddingCake

--- src/main/java/se/salt/chapter9/WeddingCake.java
+++ src/main/java/se/salt/chapter9/WeddingCake.java
@@ -1,8 +1,12 @@
 package se.salt.chapter9;
 
-public class WeddingCake extends Cake{
+public class WeddingCake extends Cake {
 
     protected int tiers;
+
+    public WeddingCake() {
+        super("Chocolate", 500);
+    }
 
     public int getTiers() {
         return tiers;
