ed9a562e1af43ab58cec8a31d577055a2bd3eadb
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-17 22:37:15 +0100

    Creating a subclass WeddingCake that extends Cake

---
+++ src/main/java/se/salt/chapter9/WeddingCake.java
@@ -0,0 +1,14 @@
+package se.salt.chapter9;
+
+public class WeddingCake extends Cake{
+
+    protected int tiers;
+
+    public int getTiers() {
+        return tiers;
+    }
+
+    public void setTiers(int tiers) {
+        this.tiers = tiers;
+    }
+}
