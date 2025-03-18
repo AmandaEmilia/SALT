a85d97fef8d3047a621387194f4b23ab8a32b3dc
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-18 14:57:41 +0100

    Creating a set of cars

---
+++ src/main/java/se/salt/chapter12/Collections.java
@@ -0,0 +1,27 @@
+package se.salt.chapter12;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Collections {
+
+    public static void main(String[] args) {
+
+        howToUseSet();
+
+    }
+
+    private static void howToUseSet() {
+        Set car = new HashSet();
+        car.add("Volvo");
+        car.add("BMW");
+        car.add("SEAT");
+        car.add("Saab");
+        car.add("Mini");
+        car.add("Volvo");
+
+        System.out.println(car.size()); // 5 bc all elements should be unique
+        System.out.println(car);
+
+    }
+}
