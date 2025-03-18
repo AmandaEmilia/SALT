7122130cf49b8262760c95b7f0907fb055cd3e3d
AmandaEmilia <amanda.e.strom@gmail.com>
2025-03-18 15:05:10 +0100

    Creating a ArrayList

--- src/main/java/se/salt/chapter12/Collections.java
+++ src/main/java/se/salt/chapter12/Collections.java
@@ -1,14 +1,31 @@
 package se.salt.chapter12;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Collections {
 
     public static void main(String[] args) {
 
-        howToUseSet();
+        //howToUseSet();
+        howToUseList();
 
+    }
+
+    private static void howToUseList() {
+        List car = new ArrayList<>();
+        car.add("Volvo");
+        car.add("BMW");
+        car.add("SEAT");
+        car.add("Saab");
+        car.add("Mini");
+        car.add("Volvo");
+
+        System.out.println(car.size()); // 6 bc all elements counts even if they are duplicates.
+        System.out.println(car.get(1)); //index 1 will be BMW
+        System.out.println(car);
     }
 
     private static void howToUseSet() {
@@ -24,7 +41,7 @@ public class Collections {
         System.out.println(car);
 
         var i = car.iterator();
-        while (i.hasNext()){
+        while (i.hasNext()) {
             System.out.println(i.next());
         }
 
